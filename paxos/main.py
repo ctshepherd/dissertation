df210d662aa42af1fc312f5f335d1b5936f5ed2f
Charlie Shepherd <cs648@cam.ac.uk>
2012-11-13 19:33:59 +0000

    Add some more Agents to the system

--- paxos/main.py
+++ paxos/main.py
@@ -2,11 +2,19 @@ from agent import Proposer, Learner, Acceptor
 from protocol import reactor
 
 p1 = Proposer()
+p2 = Proposer()
 a1 = Acceptor()
+a2 = Acceptor()
+a3 = Acceptor()
+a4 = Acceptor()
 l1 = Learner()
+l2 = Learner()
+l3 = Learner()
 
 print "Started"
 p1.run(1)
+p2.run(1)
+p1.run(3)
 
 def run():
     reactor.run()

e82b891f4b8355ed773ec6e3c91c7e062b0d4efd
Charlie Shepherd <cs648@cam.ac.uk>
2012-12-17 15:02:23 +0000

    Add paxos.proposal tests

--- paxos/test/test_proposal.py
+++ paxos/test/test_proposal.py
@@ -1,2 +1,10 @@
 from twisted.trial import unittest
-from paxos.proposal import *
+from paxos.proposal import Proposal
+
+
+class TestProposal(unittest.TestCase):
+    def test_serialize(self):
+        p = Proposal(1)
+        self.assertEqual(p.serialize(), "1,None")
+        p = Proposal(2, 3)
+        self.assertEqual(p.serialize(), "2,3")

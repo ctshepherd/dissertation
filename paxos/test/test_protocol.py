3382bb7c9ba48398d5e7dee228f40153cb0e640e
Charlie Shepherd <cs648@cam.ac.uk>
2012-12-17 13:19:50 +0000

    Extend paxos.protocol test to cover reading and writing

--- paxos/test/test_protocol.py
+++ paxos/test/test_protocol.py
@@ -1,6 +1,6 @@
 from twisted.trial import unittest
-from twisted.test import proto_helpers
-from paxos.protocol import *
+from twisted.test.proto_helpers import StringTransport as TStringTransport
+from paxos.protocol import EchoClientDatagramProtocol
 
 
 class MockAgent(object):
@@ -11,16 +11,29 @@ class MockAgent(object):
         self.msgs.append(msg)
 
 
+class StringTransport(TStringTransport):
+    def write(self, data, host=None):
+        """Wrapper around TStringTransport.write"""
+        TStringTransport.write(self, data)
+
+
 class EchoClientTest(unittest.TestCase):
     def setUp(self):
         self.proto = EchoClientDatagramProtocol()
-        self.tr = proto_helpers.StringTransport()
+        self.tr = StringTransport()
         self.ma = MockAgent()
         self.proto.parent = self.ma
         self.proto.makeConnection(self.tr)
 
-
-    def test_passthru(self):
+    def test_read(self):
         args = ("msg", "host")
         self.proto.datagramReceived(*args)
         self.assertEqual([args[0]], self.ma.msgs)
+
+    def test_write(self):
+        data = ["foo", "bar"]
+        host = "host"
+        self.proto.sendDatagram(data[0], host)
+        self.assertEqual(data[0], self.tr.value())
+        self.proto.sendDatagram(data[1], host)
+        self.assertEqual("".join(data), self.tr.value())

60250fbba5be1a3200a31e58fe58a944e755a3ef
Charlie Shepherd <cs648@cam.ac.uk>
2012-12-17 17:10:05 +0000

    Add some helpful comments to paxos.network

--- paxos/network.py
+++ paxos/network.py
@@ -1,3 +1,9 @@
+# This is a dict of {agent_type: [agents]}
 network = {}
+
+# This dict allows us to number agents properly
 network_num = {}
+
+# This is a dict of {(host, port): agent} to allow us to reverse map (purely
+# for debugging purposes
 hosts = {}

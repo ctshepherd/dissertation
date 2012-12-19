46bf9f815e6319097eef0074e58bed52fcb02be7
Charlie Shepherd <cs648@cam.ac.uk>
2012-12-19 15:07:31 +0000

    Adapt tests to deal with new sanity checking.
    Adapt paxos.test.test_message to deal with the exception raised by Prepare if it's constructed
    with a Proposal that has a value.

--- paxos/test/test_message.py
+++ paxos/test/test_message.py
@@ -1,5 +1,5 @@
 from paxos import message
-from paxos.message import parse_message, Message, Promise, Prepare, Accept
+from paxos.message import parse_message, Message, Promise, Prepare, Accept, InvalidMessageException
 from paxos.proposal import Proposal
 from paxos.test import TestCase
 
@@ -36,20 +36,33 @@ class TestParseMessage(FMTestCase):
 
 class TestMessageTypes(TestCase):
     """Test that we can do all of the message types we want"""
+
+    def _roundtrip(self, args, msg_type, msg_class):
+        """Helper function for test_deserialize"""
+        m = msg_class(Proposal(*args))
+        res = parse_message("%s:%s" % (msg_type, Proposal(*args).serialize()))
+        self.assertEqual(m, res)
+
+    def _roundtrip_s(self, args, msg_class):
+        """Helper function for test_serialization"""
+        fm_orig = msg_class(Proposal(*args))
+        fm_new = parse_message(fm_orig.serialize())
+        self.assertEqual(fm_orig, fm_new)
+
     def test_deserialize(self):
         for args in ((1,), (2, 3), (4, 5)):
-            for (msg_type, msg_class) in (("prepare", Prepare), ("promise", Promise), ("accept", Accept)):
-                m = msg_class(Proposal(*args))
-                res = parse_message("%s:%s" % (msg_type, Proposal(*args).serialize()))
-                self.assertEqual(m, res)
+            for (msg_type, msg_class) in (("promise", Promise), ("accept", Accept)):
+                self._roundtrip(args, msg_type, msg_class)
+        # We have to do Prepare separately because it doesn't take values
+        self._roundtrip((1,), "prepare", Prepare)
 
     def test_serialization(self):
         # Try to go the whole round trip
         for args in ((1,), (2, 3), (4, 5)):
-            for (msg_type, msg_class) in (("prepare", Prepare), ("promise", Promise), ("accept", Accept)):
-                fm_orig = msg_class(Proposal(*args))
-                fm_new = parse_message(fm_orig.serialize())
-                self.assertEqual(fm_orig, fm_new)
+            for msg_class in (Promise, Accept):
+                self._roundtrip_s(args, msg_class)
+        # We have to do Prepare separately because it doesn't take values
+        self._roundtrip_s((1,), Prepare)
 
 
 class TestMessage(TestCase):
@@ -58,3 +71,9 @@ class TestMessage(TestCase):
         self.assertNotEqual(Prepare(Proposal(1)), FakeMessage(Proposal(1)))
         self.assertNotEqual(FakeMessage(Proposal(1)), object())
         self.assertNotEqual(object(), FakeMessage(Proposal(1)))
+
+
+class TestPrepare(TestCase):
+    def test_sanity(self):
+        """Test that we don't allow prepare messages with values"""
+        self.assertRaises(InvalidMessageException, Prepare, Proposal(1, 1))

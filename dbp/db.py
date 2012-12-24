3c34a6d71dd382d9567d9b12503165ce4c97326d
Charlie Shepherd <cs648@cam.ac.uk>
2012-12-24 17:31:44 +0000

    Add Op class to dbp.dbp.
    Add some Op classes to dbp.dp, a way of representing operations.

--- dbp/db.py
+++ dbp/db.py
@@ -7,10 +7,110 @@ the moment it only supports get and set operations but will support more later.
 from paxos.util import dbprint
 
 
+class InvalidOp(Exception):
+    """Invalid serialization of an Op"""
+
+
+class Op(object):
+    """Database operation"""
+    op_name = "op"
+    args = None
+
+    def __init__(self, s, args):
+        self.statement = s
+        self.args = args
+
+    def perform_op(self, db):
+        raise NotImplementedError("perform_op: %s" % self)
+
+    def __str__(self):
+        return "%s(%s)" % (self.op_name, self.args)
+
+    def __repr__(self):
+        return "<%s @ %#lx>" % (str(self), id(self))
+
+    def __eq__(self, other):
+        if not isinstance(other, Op):
+            return NotImplemented
+        return (self.op_name == other.op_name and
+                self.args    == other.args)
+
+    def __ne__(self, other):
+        return not self == other
+
+
+class NOP(Op):
+    """NOP operation - does nothing."""
+    op_name = "nop"
+    num_args = 0
+
+    def __init__(self, s, args):
+        super(NOP, self).__init__(s, args)
+
+    def perform_op(self, db):
+        pass
+
+
+class Get(Op):
+    """Get operation - get an attribute from a DB"""
+    op_name = "get"
+    num_args = 1
+
+    def __init__(self, s, args):
+        super(Get, self).__init__(s, args)
+        self.key = self.args[0]
+
+    def perform_op(self, db):
+        return db.get(self.key)
+
+
+class Set(Op):
+    """Set operation - set an attribute on a DB"""
+    op_name = "set"
+    num_args = 2
+
+    def __init__(self, s, args):
+        super(Set, self).__init__(s, args)
+        self.key, self.value = self.args
+
+    def perform_op(self, db):
+        return db.set(self.key, self.value)
+
+
+ops = {
+    NOP.op_name: NOP,
+    Get.op_name: Get,
+    Set.op_name: Set,
+}
+
+
+def parse_op(s):
+    """Parse a string containing an op serialization and return an Op class.
+
+    Raises InvalidOp if s is not a valid op.
+    """
+    try:
+        op_name = s[:3]
+        kls = ops[op_name]
+        paran = s[3:]
+        if paran[0] != '(' or paran[-1] != ')':
+            raise InvalidOp(s)
+        p_args = paran[1:-1]
+        if not p_args:
+            args = []
+        else:
+            args = p_args.split(',')
+        if len(args) != kls.num_args:
+            raise InvalidOp(s)
+        return kls(s, args)
+    except (KeyError, IndexError):
+        raise InvalidOp(s)
+
+
 class DB(object):
     """Database backing store class"""
-    def __init__(self):
-        self._db = {}
+    def __init__(self, start=()):
+        self._db = dict(start)
 
     def set(self, key, val):
         """Set a key in the database to equal val."""
--- dbp/test/test_db.py
+++ dbp/test/test_db.py
@@ -1,4 +1,4 @@
-from dbp.db import DB
+from dbp.db import DB, NOP, Get, Set, parse_op, InvalidOp
 from paxos.test import TestCase
 
 
@@ -7,4 +7,51 @@ class TestDB(TestCase):
         db = DB()
         db.set("foo", "bar")
         self.assertEqual(db.get("foo"), "bar")
+
+    def test_get(self):
+        db = DB({"foo": "bar"})
+        self.assertEqual(db.get("foo"), "bar")
         self.assertRaises(KeyError, db.get, "foobar")
+
+
+class TestOps(TestCase):
+    def test_nop(self):
+        db = DB()
+        d = dict(db._db)
+        n = NOP("nop()", ())
+        n.perform_op(db)
+        self.assertEqual(d, db._db)
+
+    def test_get(self):
+        db = DB({"foo": "bar"})
+        n = Get("get(foo)", ["foo"])
+        ret = n.perform_op(db)
+        self.assertEqual(ret, "bar")
+
+    def test_set(self):
+        db = DB({"foo": "bar"})
+        n = Set("set(foo,baz)", ("foo", "baz"))
+        n.perform_op(db)
+        self.assertEqual(db.get("foo"), "baz")
+
+    def test_eq(self):
+        self.assertEqual(Set("set(foo,baz)", ["foo", "baz"]), Set("set(foo,baz)", ["foo", "baz"]))
+        self.assertNotEqual(Set("set(bar,baz)", ["bar", "baz"]), Set("set(foo,baz)", ["foo", "baz"]))
+        self.assertNotEqual(Get("get(foo)", ["foo"]), Set("set(foo,baz)", ["foo", "baz"]))
+
+
+class TestParseOp(TestCase):
+    def test_parse(self):
+        self.assertEqual(parse_op("get(foo)"), Get("get(foo)", ["foo"]))
+        self.assertEqual(parse_op("set(bar,baz)"), Set("set(bar,baz)", ["bar", "baz"]))
+        self.assertEqual(parse_op("nop()"), NOP("nop()", []))
+
+    def test_raises(self):
+        self.assertRaises(InvalidOp, parse_op, "get()")
+        self.assertRaises(InvalidOp, parse_op, "get(foo,bar)")
+        self.assertRaises(InvalidOp, parse_op, "set()")
+        self.assertRaises(InvalidOp, parse_op, "set(foo)")
+        self.assertRaises(InvalidOp, parse_op, "nop(foo)")
+        self.assertRaises(InvalidOp, parse_op, "foo(bar)")
+        self.assertRaises(InvalidOp, parse_op, "get")
+        self.assertRaises(InvalidOp, parse_op, "foo")

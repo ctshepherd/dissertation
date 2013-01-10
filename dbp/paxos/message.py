c4ff4f3fe2a9e2985460f1b34ab434a7a19eb4f7
Charlie Shepherd <cs648@cam.ac.uk>
2013-01-10 00:34:35 +0000

    Make sure parse_message raises InvalidMessageException

--- dbp/paxos/message.py
+++ dbp/paxos/message.py
@@ -14,13 +14,16 @@ def parse_message(msg):
     This takes a string (probably received off the network) and returns the
     corresponding Message object.
     """
-    mtype, p = msg.split(':')
-    pt = p.split(',')  # proposal value tuple
-    m = message_types[mtype](
-        Proposal(literal_eval(pt[0]),   # literal_eval is a safe version of eval,
-                 literal_eval(pt[1])))  # that only works for literal values
-    dbprint("converting '%s' to '%s'" % (msg, m), level=1)
-    return m
+    try:
+        mtype, p = msg.split(':')
+        pt = p.split(',')  # proposal value tuple
+        m = message_types[mtype](
+            Proposal(literal_eval(pt[0]),   # literal_eval is a safe version of eval,
+                    literal_eval(pt[1])))  # that only works for literal values
+        dbprint("converting '%s' to '%s'" % (msg, m), level=1)
+        return m
+    except (KeyError, ValueError), e:
+        raise InvalidMessageException("invalid msg '%s' (%r)" % (msg, e))
 
 
 class Message(object):

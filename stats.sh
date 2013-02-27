17d1049c052cb68bc8de9f4c30a09f3d1bf3a16b
Charlie Shepherd <cs648@cam.ac.uk>
2013-02-27 16:41:23 +0000

    Add script for generating stats

---
+++ stats.sh
@@ -0,0 +1,15 @@
+export PYTHONPATH="/home/cs648/dissertation"
+
+#for transaction_number in 50 500 5000; do
+for transaction_number in 5000; do
+	for run_number in `seq 1 10`; do
+		prefix="${transaction_number}:${run_number}:"
+		log_prefix="${transaction_number}_${run_number}"
+		echo $prefix
+		bin/pserv &>logs/pserv_${log_prefix}.log &
+		bin/peval --amount ${transaction_number} -b 10000 -p 10001 -D 3 &>logs/peval_${log_prefix}.log
+		kill $!
+		sleep 1
+		echo
+	done
+done

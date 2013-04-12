ec0de69a094a73bb59a206d920c05ca655f91db5
Charlie Shepherd <cs648@cam.ac.uk>
2013-04-12 22:40:13 +0100

    Fix stat scripts

--- lat/lat_stat.sh
+++ lat/lat_stat.sh
@@ -1,12 +1,15 @@
 export PYTHONPATH="/home/cs648/dissertation"
 
+EVAL=../bin/eval
+AVG=../avg.py
+
 metric=lat
 node_num=$1
-for mode in p o m; do
-	for write_num in `seq 1 $node_num`; do
+for mode in o m; do
+	for write_num in `seq 1 12`; do
 		res=$({ for run_number in `seq 1 5`; do
-			bin/eval -m $metric -n $node_num -o 5 -D 5 -w $write_num -M $mode
-		done } | python avg.py)
+			$EVAL -m $metric -n $node_num -o 5 -D 5 -w $write_num -M $mode
+		done } | python $AVG)
 		echo "$write_num,$mode,$res"
 	done
 done
--- thru/thru_stat.sh
+++ thru/thru_stat.sh
@@ -1,13 +1,16 @@
 export PYTHONPATH="/home/cs648/dissertation"
 
+EVAL=../bin/eval
+AVG=../avg.py
+
 metric=thru
 node_num=$1
 #20 100; do
 for mode in p o m; do
 	for write_num in `seq 1 $node_num`; do
 		res=$({ for run_number in `seq 1 5`; do
-			bin/eval -m $metric -n $node_num -o 5 -D 5 -w $write_num -M $mode
-		done } | python avg.py)
+			$EVAL -m $metric -n $node_num -o 5 -D 5 -w $write_num -M $mode
+		done } | python $AVG)
 		echo "$node_num,$write_num,$mode,$res"
 	done
 done

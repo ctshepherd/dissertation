8c96642659ec29ec55cf872e2dc49bae02207eee
Charlie Shepherd <cs648@cam.ac.uk>
2013-04-12 21:38:16 +0100

    Add latency to evaluation

--- lat_stat.sh
+++ lat_stat.sh
@@ -1,13 +1,12 @@
 export PYTHONPATH="/home/cs648/dissertation"
 
-metric=thru
+metric=lat
 node_num=$1
-#20 100; do
 for mode in p o m; do
 	for write_num in `seq 1 $node_num`; do
 		res=$({ for run_number in `seq 1 5`; do
 			bin/eval -m $metric -n $node_num -o 5 -D 5 -w $write_num -M $mode
 		done } | python avg.py)
-		echo "$node_num,$write_num,$mode,$res"
+		echo "$write_num,$mode,$res"
 	done
 done
